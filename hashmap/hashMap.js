91a1e2421d8db9395fb096f55465c2593388784d
David Oquendo <davidoqu23@gmail.com>
2022-08-08 21:43:52 -0500

    added the get method

--- hashmap/hashMap.js
+++ hashmap/hashMap.js
@@ -25,7 +25,21 @@ In this implementation, we'll use arrays instead of objects but they work simila
     let address = this._hash(key);
     if(!this.data[address]) {
       this.data[address] = [];
-    }
+    } 
     this.data[address].push([key, value]);
+  }
+
+  // This "get" method is used to retrieve the value from a given key
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if(currentBucket) {
+      for(let i = 0; i < currentBucket.length; i++) {
+        if(currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
   }
  }
