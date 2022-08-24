9256b15449ca18f62082cfaf0e57b7f48b9de7ca
David Oquendo <davidoqu23@gmail.com>
2022-08-23 20:55:24 -0500

    Started implementing the Stack data structure

---
+++ stacks_queues/stack.js
@@ -0,0 +1,20 @@
+/*
+
+In this stack implementation, I will build a Stack data structure using both Arrays and Linked Lists
+
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+}
