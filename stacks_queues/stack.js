8273bd8e22ab5e0d8dcbda91e01893211dd29013
David Oquendo <davidoqu23@gmail.com>
2022-08-23 20:58:48 -0500

    Added the peek method to the Stack data structure

--- stacks_queues/stack.js
+++ stacks_queues/stack.js
@@ -1,6 +1,7 @@
 /*
 
 In this stack implementation, I will build a Stack data structure using both Arrays and Linked Lists
+Remeber, Stacks abide by the LIFO (last in, first out) rule
 
 */
 
@@ -12,9 +13,17 @@ class Node {
 }
 
 class Stack {
+  // This is the inital function that will be run to create the "array" and its two properties
   constructor() {
     this.top = null;
     this.bottom = null;
     this.length = 0;
   }
+
+  // This "peek" method allows us to see the very top item in our respective stack 
+  peek() {
+    return this.top;
+  }
 }
+
+const myStack = new Stack();
