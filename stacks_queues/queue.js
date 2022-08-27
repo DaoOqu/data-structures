43c09d9229036ecc199e17f20ada3815264bd29e
David Oquendo <davidoqu23@gmail.com>
2022-08-26 23:24:32 -0500

    Added the enqueue method

--- stacks_queues/queue.js
+++ stacks_queues/queue.js
@@ -1,9 +1,16 @@
 /*
 
-In this queue implementation, I will build a Queue data structure using ONLY ARRAYS
+In this queue implementation, I will build a Queue data structure using ONLY LINKED LISTS
 Remeber, Stacks abide by the FIFO (first in, first out) rule
 
 */
+
+class Node {
+  constructor() {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class Queue {
   constructor() {
@@ -15,6 +22,20 @@ class Queue {
   // This method "peek" allows us to look at the first item in our queue
   peek() {
     return this.first;
+  }
+
+  // This method "enqueue" allows us to add to the end of our queue
+  enqueue(value) {
+    const newNode = new Node(value);
+    if(this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
   }
 }
 
