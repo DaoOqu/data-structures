526bc3912ac505dff417df8dca6df0828d57d7a3
David Oquendo <davidoqu23@gmail.com>
2022-08-29 20:04:12 -0500

    Added Binary Search Tree data strucutre and started implementing it

---
+++ binary_trees/bst.js
@@ -0,0 +1,21 @@
+/*
+Binary Search Trees are comprised of nodes. With that being said, we can create a binary search tree
+using similar properties as Linked Lists
+*/
+
+class Node {
+  constructor() {
+    this.left = null;
+    this.right = null;
+    this.value = this.value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+}
+
+const tree = new BinarySearchTree();
