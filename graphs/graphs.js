218d5a47b70672c8a728387f8a95a24b4d49d370
David Oquendo <davidoqu23@gmail.com>
2022-09-05 17:33:05 -0500

    Added helper function that helps visualize graphs

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -20,9 +20,18 @@ class Graph {
     // undirected graph
   }
 
-  // connections
+  // This is a helper function that lets us visualize the graph
   showConnections() {
-
+    const allNodes = Object.keys(this.adjacentList);
+    for(let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for(vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
   }
 }
 
