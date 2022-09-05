ca3b3fae9b18a712d4dee6c085e3d301d18d544c
David Oquendo <davidoqu23@gmail.com>
2022-09-04 21:18:30 -0500

    Option 1 implemented inside remove method

--- binary_trees/bst.js
+++ binary_trees/bst.js
@@ -80,7 +80,13 @@ class BinarySearchTree {
           if(parentNode === null) {
             this.root = currentNode.left;
           } else {
-            
+            // if parent > current value, make current left child a child of parent
+            if(currentNode.value < parentNode.value) {
+              parentNode.left = currentNode.left;
+            } else if(currentNode.value > parentNode.value) {
+              // if parent < current value, make left child a right child of parent
+              parentNode.right = currentNode.left;
+            }
           }
         }
       }
