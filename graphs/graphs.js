3f83b3b0725070ebc6e096279ac1f7bad7420eb1
David Oquendo <davidoqu23@gmail.com>
2022-09-05 17:28:04 -0500

    Started implementing Graphs. Finished the template

---
+++ graphs/graphs.js
@@ -0,0 +1,30 @@
+/*
+In this Graphs implementation, I will only critical graph methods.
+This will be an implementation of an unweighted, undirected graph using an adjacenecy list
+The adjacency list will be using a hashtable, or in JS, an object
+*/
+
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+
+  // add vertex
+  addVertex(node) {
+
+  }
+
+  // add edge
+  addEdge(node1, node2) {
+    // undirected graph
+  }
+
+  // connections
+  showConnections() {
+
+  }
+}
+
+
+const myGraph = new Graph();
